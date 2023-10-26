b5ccc26251bf9130ce274267fe47999c0b84bb6b
MomoCrash <44437470+MomoCrash@users.noreply.github.com>
2023-10-26 10:55:26 +0200

    Finish

--- js/action.js
+++ js/action.js
@@ -7,12 +7,16 @@ const questionsTypeEnum = Object.freeze({
     "Lines": 3,
   });
 
+var POSITIVE_MULTIPLIER = 2;
+var NEGATIVE_MULTIPLIER = 1;
+
 var IS_FIRST = true;
 var IS_RESET = false;
 var IS_FINISH = false;
 
 // All questions and progression
 var QUESTIONS = [];
+var ANWSERED_QUESTION = [];
 var CURRENT_QUESTION = null;
 var QUESTION_NUMBER = 10;
 var QUESTION_PROGRESSION = 0;
@@ -54,6 +58,24 @@ class Question{
     }
 }
 
+$.getJSON("js/questions.json", function(content) {
+
+    let questions = content
+
+    for (let index = 0; index < questions.length; index++) {
+
+        const element = questions[index];
+        
+        let type = questionsTypeEnum[element.questionType]
+        let question = element.question
+        let answers = element.answers
+        let validAnswers = element.validAnswers
+
+        new Question(type, question, answers, validAnswers);
+    }
+
+});
+
 $("#question-reset").click(function() {
 
     CURRENT_QUESTION=null;
@@ -61,6 +83,7 @@ $("#question-reset").click(function() {
     SCORE=0;
 
     SELECTED_ANSWER = [];
+    ANWSERED_QUESTION = [];
 
     IS_FEEDBACK=false;
     FEEDBACK_QUESTION=false;
@@ -112,7 +135,16 @@ function displayQuestion(question) {
 
 function updateCard(isProgression) {
     $('#question-card').empty();
-    CURRENT_QUESTION = QUESTIONS[Math.floor(Math.random()*QUESTIONS.length)]
+
+    let questionIndex = Math.floor(Math.random()*QUESTIONS.length);
+    let tryCount = 0;
+    while (ANWSERED_QUESTION.includes(questionIndex) && tryCount < 10) {
+        questionIndex = Math.floor(Math.random()*QUESTIONS.length);
+        tryCount++; 
+    }
+    ANWSERED_QUESTION.push(questionIndex);
+    CURRENT_QUESTION = QUESTIONS[questionIndex]
+
     displayQuestion(CURRENT_QUESTION)
 
     if (isProgression) {
@@ -122,22 +154,28 @@ function updateCard(isProgression) {
     }
 }
 
-$.getJSON("js/questions.json", function(content) {
+$("#question-easy").click(function() {
+    $('#question-easy').attr('style', 'background-color: #ffd000');
+    $('#question-medium').attr('style', 'background-color: #6c757d');
+    $('#question-hard').attr('style', 'background-color: #6c757d');
+    POSITIVE_MULTIPLIER=2;
+    NEGATIVE_MULTIPLIER=1;
+});
 
-    let questions = content
+$("#question-medium").click(function() {
+    $('#question-easy').attr('style', 'background-color: #6c757d');
+    $('#question-medium').attr('style', 'background-color: #ffd000');
+    $('#question-hard').attr('style', 'background-color: #6c757d');
+    POSITIVE_MULTIPLIER=1;
+    NEGATIVE_MULTIPLIER=1;
+});
 
-    for (let index = 0; index < questions.length; index++) {
-
-        const element = questions[index];
-        
-        let type = questionsTypeEnum[element.questionType]
-        let question = element.question
-        let answers = element.answers
-        let validAnswers = element.validAnswers
-
-        new Question(type, question, answers, validAnswers);
-    }
-
+$("#question-hard").click(function() {
+    $('#question-easy').attr('style', 'background-color: #6c757d');
+    $('#question-medium').attr('style', 'background-color: #6c757d');
+    $('#question-hard').attr('style', 'background-color: #ffd000');
+    POSITIVE_MULTIPLIER=1;
+    NEGATIVE_MULTIPLIER=2;
 });
 
 // Check click on the Question button
@@ -145,16 +183,17 @@ $("#question-next").click(function(){
 
     if (IS_FIRST) {
 
-        $("#progress-bar").append('<div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="10">'
-        + '<div id="question-progress" class="progress-bar progress-bar-striped progress-bar-animated" style="width: 0%;">0%</div></div>"');
+        $("#progress-bar").append('<div class="progress" role="progressbar" aria-label="Animated striped example" aria-valuenow="0" aria-valuemin="0" aria-valuemax="10"><div id="question-progress" class="progress-bar progress-bar-striped progress-bar-animated" style="width: 0%;">0%</div></div>"');
         $("#question-next").text("Correction");
+        $('#question-difficulty').empty();
         IS_FIRST = false;
         updateCard(false)
 
     } else if (IS_RESET) {
 
         IS_RESET=false;
-        updateCard(false)
+        QUESTION_PROGRESSION = 0;
+        updateCard(true)
         $("#question-next").text("Correction");
 
     } else if(IS_FEEDBACK) {
@@ -162,8 +201,31 @@ $("#question-next").click(function(){
         $("#question-next").text("Correction");
         IS_FEEDBACK = false;
         QUESTION_PROGRESSION += 1
+
         // Set a new question on page
-        updateCard(true)
+        if (QUESTION_PROGRESSION==QUESTION_NUMBER) {
+            $("#question-next").text("Finir le quiz");
+            IS_FINISH = true;
+        } else {
+            updateCard(true)
+        }
+
+    } else if (IS_FINISH) {
+
+        console.log("Finis")
+
+        $("#question-title").text("Resultat");
+        $("#question-subtitle").text("Quiz en difficulte moyenne");
+
+        $('#question-card').empty();
+        $('#question-card').text('Vous avez : ' + SCORE + ' de score !')
+
+        $("#question-next").text("Commencez le Quiz !")
+
+        let progression = (QUESTION_PROGRESSION * 100)/QUESTION_NUMBER;
+        $('#question-progress').attr('style', 'width: ' + progression + '%').text(progression + "%")
+        IS_FINISH=false;
+        IS_RESET=true;
 
     } else {
 
@@ -188,13 +250,13 @@ $("#question-next").click(function(){
         for (let index = 0; index < currentAnwsers[0].length; index++) {
             const element = currentAnwsers[0][index];
             console.log("+1");
-            SCORE += 1;
+            SCORE += 1*POSITIVE_MULTIPLIER;
         }
         for (let index = 0; index < currentAnwsers[1].length; index++) {
             const element = currentAnwsers[1][index];
             console.log("-1");
-            if (SCORE >= 1) {
-                SCORE -= 1;
+            if (SCORE >= 1*NEGATIVE_MULTIPLIER) {
+                SCORE -= 1*NEGATIVE_MULTIPLIER;
             }
         }
 
@@ -209,11 +271,11 @@ $("#question-next").click(function(){
         for (let index = 0; index < CURRENT_QUESTION.answers.length; index++) {
             const element = CURRENT_QUESTION.answers[index];
             if (currentAnwsers[0].includes(index) && CURRENT_QUESTION.validAnswers.includes(index)) {
-                $("#question-card").first().append('<li id="anwser_' + index + '" class="list-group-item" style="background-color: #00BC1B;><div class="card-answer">' + element + ' +1</div></li>');
+                $("#question-card").first().append('<li id="anwser_' + index + '" class="list-group-item" style="background-color: #00BC1B;><div class="card-answer">' + element + " +" + 1*POSITIVE_MULTIPLIER + '</div></li>');
             } else if (currentAnwsers[1].includes(index)) {
-                $("#question-card").first().append('<li id="anwser_' + index + '" class="list-group-item" style="background-color: #C0392B;  text-decoration: line-through;">' + element + ' -1</div></li>');
+                $("#question-card").first().append('<li id="anwser_' + index + '" class="list-group-item" style="background-color: #C0392B;  text-decoration: line-through;">' + element + " -" + 1*NEGATIVE_MULTIPLIER + '</div></li>');
             } else if (CURRENT_QUESTION.validAnswers.includes(index)) {
-                $("#question-card").first().append('<li id="anwser_' + index + '" class="list-group-item" style="background-color: #00BC1B;">' + element + ' -1</div></li>');
+                $("#question-card").first().append('<li id="anwser_' + index + '" class="list-group-item" style="background-color: #00BC1B;">' + element + " -" + 1*NEGATIVE_MULTIPLIER + '</div></li>');
             } else {
                 $("#question-card").first().append('<li id="anwser_' + index + '" class="list-group-item" style="background-color: #fff;">' + element + '</div></li>');
             }
